d61ead255e1b6cb393dac6ba08501e85bb20acd6
wang dong <jacob.wd8816@gmail.com>
2024-11-23 21:02:34 +0800

    fix deploy url

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ITJacob/bailing-game.git main:gh-pages
+git push -f git@github.com:ITJacob/bailin_babylon.git main:gh-pages
 
 cd -
